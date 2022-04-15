abc02e903890e733b69c81ca285106a75f63d540
Shamimgardobuya <shamimobuya@gmail.com>
2022-04-15 23:00:51 +0300

    made calclator

---
+++ src/main.kt
@@ -0,0 +1,32 @@
+fun main(args:Array<String>){
+    var a = 4
+    var b = 8
+
+  println("Enter operator type,either +,-,/,or *")
+    val operator = readLine() //it reads the type of operator that you will input.
+    val result= when (operator) {
+        "+"->a+b
+        "-"->a-b
+        "/"->a/b
+        "*"->a*b
+        else-> "$operator is invalid operator"
+    }
+
+   println(result)
+
+
+    }
+fun us(args: Array<String>){
+    var p = 3
+    while (p<=10){
+        println("line $p")
+        ++p    // represents an increment.
+
+    }
+
+}
+
+
+
+
+//GIven a list of 10 strings
